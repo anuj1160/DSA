9d291d4820f00c6f6bae5506f814c20c62d1e042
Anuj Jain <anujjain8044@gmail.com>
2022-03-09 08:30:58 +0530

    traversal

---
+++ tree/LevelOrderTraversal.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    struct Node *left;
+    struct Node *right;
+    int data;
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+void InorderTraversal(struct Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    InorderTraversal(root->left);
+    cout << root->data << "->";
+    InorderTraversal(root->right);
+}
+void LevelOrderTraversal(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    queue<Node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        Node *curr = q.front();
+        q.pop();
+        cout << curr->data << " ";
+        if (curr->left != NULL)
+        {
+            q.push(curr->left);
+        }
+        if (curr->right != NULL)
+        {
+            q.push(curr->right);
+        }
+    }
+}
+int main()
+{
+    struct Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(5);
+    root->right->right = new Node(8);
+    root->left->right = new Node(20);
+    root->right->left = new Node(98);
+    root->left->left = new Node(78);
+    //    InorderTraversal(root);
+    LevelOrderTraversal(root);
+    return 0;
+}
