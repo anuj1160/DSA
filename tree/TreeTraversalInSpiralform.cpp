fe371e1a98c88bed16e7f4d05edc02783da3fd77
Anuj Jain <anujjain8044@gmail.com>
2022-04-08 08:23:50 +0530

    gfg course

---
+++ tree/TreeTraversalInSpiralform.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    struct Node *left;
+    struct Node *right;
+    int data;
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+void TraversalInSpiral(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    queue<Node *> q;
+    stack<int> s;
+    bool reverse = false;
+    q.push(root);
+    while (q.empty() == false)
+    {
+        int count = q.size();
+        for (int i = 0; i < count; i++)
+        {
+            Node *curr = q.front();
+            q.pop();
+            if (reverse)
+            {
+                s.push(curr->data);
+            }
+            else
+            {
+                cout << curr->data<<" ";
+            }
+            if (curr->left != NULL)
+            {
+                q.push(curr->left);
+            }
+            if (curr->right != NULL)
+            {
+                q.push(curr->right);
+            }
+        }
+        if(reverse)
+        {
+            while(s.empty()==false)
+            {
+                cout<<s.top()<<" ";
+                s.pop();
+            }
+        }
+        reverse=!reverse;
+        cout<<endl;
+    }
+}
+void InorderTraversal(struct Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    InorderTraversal(root->left);
+    cout << root->data << "->";
+    InorderTraversal(root->right);
+}
+int main()
+{
+    struct Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(5);
+    root->right->right = new Node(8);
+    root->left->right = new Node(20);
+    root->right->left = new Node(98);
+    root->left->left = new Node(78);
+    //    InorderTraversal(root);
+    TraversalInSpiral(root);
+    return 0;
+}
